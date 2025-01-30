c56061db25e9b4af77888a2922ec01d05ff7ecaf
mark aries <markariesdelfino@gmail.com>
2025-01-30 12:14:50 +0800

    32

--- routes/channels.php
+++ routes/channels.php
@@ -11,8 +11,9 @@ use Illuminate\Support\Facades\Broadcast;
 | application supports. The given channel authorization callbacks are
 | used to check if an authenticated user can listen to the channel.
 |
-*/
+*/dsadasd
 
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+dasda
